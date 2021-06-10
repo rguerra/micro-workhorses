7d286dd2ac6e0ae34b7106e2028e7a0e41eb347a
rguerra <rguerr@www-1>
2021-06-10 13:30:42 +0000

    Small step

--- push-mongodb/src/index.js
+++ push-mongodb/src/index.js
@@ -13,3 +13,25 @@ importCsvFile("./data/earthquakes.csv")
         console.error("An error occurred.");
         console.error(err.stack);
     });
+
+const express = require("express");
+
+const app = express();
+if(!process.env.PORT) {
+	throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
+}
+const PORT = process.env.PORT;
+
+//
+// Registers a HTTP GET route.
+//
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+//
+// Starts the HTTP server.
+//
+app.listen(PORT, () => {
+    console.log(`First example app listening on port ${PORT}, point your browser at http://localhost:${PORT}`);
+});  
